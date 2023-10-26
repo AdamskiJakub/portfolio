98a96eb9a96950563bc3a86ade32d01011e57443
AdamskiJakub <105269736+AdamskiJakub@users.noreply.github.com>
2023-10-26 02:34:12 +0200

    finishing home page

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,7 @@ const Home = () => {
     <div className="bg-primary/60 h-full">
       <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
         <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
+          {/*  TEXT  */}
           <motion.h1
             variants={fadeIn("down", 0.2)}
             initial="hidden"
@@ -45,11 +46,29 @@ const Home = () => {
           </motion.div>
         </div>
       </div>
-      <div>image</div>
+      {/* Image*/}
       <div className="w-[1200px] h-full absolute right-0 bottom-0">
-        <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge">
-          image
-        </div>
+        {/* Background Image */}
+        <motion.div
+          variants={fadeIn("left", 0.6)}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+          transition={{ duration: 1, ease: "easeInOut" }}
+          className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat 
+        w-full h-full absolute mix-blend-color-dodge translate-z-0"
+        ></motion.div>
+        <div>Particles</div>
+        <motion.div
+          variants={fadeIn("up", 0.6)}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+          transition={{ duration: 1, ease: "easeInOut" }}
+          className="w-full h-full max-w-[600px] max-h-[600px] absolute -bottom-32 lg:bottom-0 lg:right-[12%]"
+        >
+          <Avatar />
+        </motion.div>
       </div>
     </div>
   );
