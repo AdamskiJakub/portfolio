863eda305a77a480a9971138561b94b469d56dbe
AdamskiJakub <porgenos32@wp.pl>
2023-11-30 23:23:38 +0100

    fix

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ const Home = () => {
   return (
     <div className="bg-primary/60 h-full xse:overflow-y-auto">
       <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
-        <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto se:mt-[-130px] sm:mt-[0px] xsm:mt-[-50px] xse:mt-[-5px]">
+        <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto se:mt-[-130px] sm:mt-[0px] xsm:mt-[-50px] xse:mt-[-40px]">
           {/*  TEXT  */}
           <motion.h1
             variants={fadeIn("down", 0.2)}
